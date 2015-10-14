0917867634b91ca6eeaf329235ea29ddb04812a5
Diskord <rickalodeon@gmail.com>
2015-10-14 19:08:07 -0400

    Update DebugFour3.java

--- DebugFour3.java
+++ DebugFour3.java
@@ -9,9 +9,9 @@ public class DebugFour3
       DebugBox box1 = new FixDebugBox();
       DebugBox box2 = new DebugBox(length, width, height);
       System.out.println("The dimensions of the first box are " + box1.showData());
-      System.out.print("  The volume of the first box is " + showVolume(box1));
+      System.out.print("  The volume of the first box is " + box1.showVolume());
       System.out.println("The dimensions of the second box are " + box2.showData());
-      System.out.print("  The volume of the second box is " + showVolume(box2));
+      System.out.print("  The volume of the second box is " + box2.showVolume());
    }
    
    public double showVolume()
